16654734f39c4ba86edb865d3561aa9d5100eaa0
Lucas Araújo <lucasaraujodesa@hotmail.com>
2023-02-15 18:08:35 -0300

    add: form state

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react'
 import styles from './Form.module.css'
 
 const Input = () => {
+
+  const [task, setTask] = useState('')
+
+  const handleTask = (event) => {
+    setTask(event.target.value)
+  }
+
   return (
     <form className={styles.box}>
-      <input className={styles.input} type="text" name="task" placeholder='Enter your task...' />
+      <input
+        value={task}
+        onChange={handleTask}
+        className={styles.input}
+        type="text"
+        name="task"
+        placeholder='Enter your task...'
+      />
       <input type="submit" value="Add" className={styles.button} />
     </form>
   )
